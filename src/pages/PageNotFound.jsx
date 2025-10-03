3e61b8c26058a044b85627eb6de62ff463d190ba
riyad-96 <riyadofficial.ide@gmail.com>
2025-10-03 12:08:30 +0600

    -

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -7,7 +7,7 @@ function PageNotFound() {
       <h1 className="fixed top-4 left-6 w-fit text-2xl font-medium">KitzoBazar</h1>
 
       <div className="mx-auto grid w-full max-w-[500px] content-center justify-items-center gap-4">
-        <span>Page not found</span>
+        <span>Page not found <span className="text-orange-400">!</span></span>
         <button className="w-[170px] rounded-full bg-(--primary) py-3 text-sm shadow" onClick={() => navigate('/', { replace: true })}>
           Home
         </button>
